4a17967c560bfd2373e2c9b2af765020b3248f24
Senanu Spring-Pearson <senanu.junk@gmail.com>
2026-01-15 22:06:02 -0800

    removed test files

--- hello.R
+++
@@ -1,9 +0,0 @@
-# Update from source repo
-usethis::use_git_config(user.name = "senanu", user.email = "senanu.pearson@gmail.com")
-
-
-print("Hello from Binder!")
-
-# this is some new text
-
-# and some more new text
